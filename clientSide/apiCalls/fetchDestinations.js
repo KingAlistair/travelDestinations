f3837a803d5aeda4a4e4a5c9e4edd1fe38459299
Alessia <98785381+amorea9@users.noreply.github.com>
2024-10-05 15:36:53 +0200

    destinations displaying in the homepage

--- clientSide/apiCalls/fetchDestinations.js
+++ clientSide/apiCalls/fetchDestinations.js
@@ -1,18 +1,38 @@
-const url = "http://127.0.0.1:3000/api/destinations"
+const url = "http://127.0.0.1:3000/api/destinations";
 
 // Delete destination by id
 export async function deleteDestination(destinationId) {
-    try {
-        const response = await fetch(`${url}/${destinationId}`, {
-            method: 'DELETE',
-        });
+  try {
+    const response = await fetch(`${url}/${destinationId}`, {
+      method: "DELETE",
+    });
 
-        if (!response.ok) {
-            throw new Error(`Error: ${response.statusText}`);
-        };
-
-        console.log('Destination deleted successfully');
-    } catch (error) {
-        console.error('There was an error deleting the destination:', error);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
     }
+
+    console.log("Destination deleted successfully");
+  } catch (error) {
+    console.error("There was an error deleting the destination:", error);
+  }
 }
+
+//FECTH all destinations
+export const getAllDestinations = async () => {
+  const response = await fetch(url);
+  return await response.json();
+};
+
+//CREATE destination
+export const createDestination = async (payload) => {
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const createdDestination = await response.json();
+  console.log("createdDestination", createdDestination);
+  return createdDestination;
+};
